5b34031229dce5eb635640f4fc9052fb3d962a26
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2024-02-19 21:35:47 -0500

    final commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// })
+// routes
+app.use("/api/products", productRoute);
+
+app.get('/', function (req, res) {
+    res.send('Hello World server updated again')
+})
+
+
+
+mongoose.connect("mongodb+srv://jeffg2221:CZ6Zf0Pb7271SZKP@backenddb.iz2gxkz.mongodb.net/Node-API?retryWrites=true&w=majority")
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => {
+    console.log("Connection failed!");
+})
